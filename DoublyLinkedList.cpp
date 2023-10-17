57be8c61d7f60ed2acc3a755ec727166af6a42dc
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-10-16 23:57:49 -0500

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -17,3 +17,11 @@ class DoublyLinkedList {
         int m_size;
         Node* m_head;
         Node* m_tail; 
+
+ void insert_very_first_node(Node* node_to_insert) {//prepending the list
+    m_head = node_to_insert;
+    m_tail = node_to_insert;
+    node_to_insert->next = nullptr;
+    node_to_insert->previous = nullptr;
+    m_size++;
+}
