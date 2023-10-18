428f11a830c94eef9a3cad3fb46285d62240f9c0
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-10-18 18:34:34 -0500

    Add files via upload

---
+++ TestStack.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "Stack.h"
+
+
+int main(){
+    Stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    DoublyLinkedList.print_list();
+}
