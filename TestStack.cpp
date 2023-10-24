2750108549ec4fe48ebd95755d4e2a51f9ee4ed0
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-10-24 18:25:44 -0500

    Update TestStack.cpp
    for loop

--- TestStack.cpp
+++ TestStack.cpp
@@ -5,7 +5,24 @@
 bool areBracketsBalanced(string expr){
     Stack<char> backing;
     for(int i = 0; i < expr.length(); i++) {
-        
+        if (backing.is_empty()) {
+                    
+            // If the stack is empty 
+            // just push the current bracket
+            backing.push(expr[i]);
+        } else if ((backing.peak() == '(' && expr[i] == ')') ||
+                   (backing.peak() == '{' && expr[i] == '}')|| 
+                   (backing.peak() == '[' && expr[i] == ']')) {
+                
+                    backing.pop();
+        } else {
+            backing.push(expr[i]);
+        }
+    }
+    if (backing.is_empty()) {
+                
+        // If stack is empty return true
+        return true;
     }
 }
 
