35e29a303724781a466d83053c058ffd44ebd062
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-11-13 14:34:32 -0600

    Update BinarySearchTreeTest.cpp
    adding the test code for the inorder, preorder, and postorder traversals

--- BinarySearchTreeTest.cpp
+++ BinarySearchTreeTest.cpp
@@ -5,3 +5,29 @@
 
 #include "BinarySearchTree.h"
 
+int main() {
+    BinarySearchTree<int> tree;
+    //std::cout << "why" << std::endl;
+    // Insert values
+    tree.insert(17);
+    tree.insert(25);
+    tree.insert(4);
+    tree.insert(88);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(5);
+    
+    // Print tree using traversals
+    std::cout << "Inorder traversal: ";
+    tree.inorder_print();
+
+    std::cout << "Preorder traversal: ";
+    tree.preorder_print();
+
+    std::cout << "Postorder traversal: ";
+    tree.postorder_print();
+
+    //system("pause");
+
+    return 0;
+}
