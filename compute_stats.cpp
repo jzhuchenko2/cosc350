30e712ff50ae29cb7d156c6b3d92a727524d636d
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-12-02 20:12:39 -0600

    Update compute_stats.cpp
    adding compte_median
    // Determine and return the median of the values in a_list.

--- compute_stats.cpp
+++ compute_stats.cpp
@@ -18,3 +18,20 @@ T compute_average(const std::list<T>& a_list) {
     // Hint: accumulation
     return std::accumulate(a_list.begin(), a_list.end(), static_cast<T>(0)) / static_cast<T>(a_list.size());
 }
+template <typename T>
+T compute_median(const std::list<T>& a_list) {
+    // Determine and return the median of the values in a_list.
+    // Hint 1: Create a vector with the same values and sort it.
+    // Hint 2: If the size of the vector is even, average the middle 2 values.
+    // If the size of the vector is not even, return the middle value.
+
+    // Using C++ Standard Library functions to compute the median
+    std::vector<T> sorted_values(a_list.begin(), a_list.end());
+    std::sort(sorted_values.begin(), sorted_values.end());
+
+    size_t size = sorted_values.size();
+    size_t middle = size / 2;
+
+    // Checking if the size is even or odd and returning the appropriate value
+    return (size % 2 == 0) ? (sorted_values[middle - 1] + sorted_values[middle]) / static_cast<T>(2) : sorted_values[middle];
+}
